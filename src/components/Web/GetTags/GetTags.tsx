bee17b1f19f3a8eec5249d2fe5b0cea5495fd231
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-04-25 19:34:24 +0900

    とりあえずmapどうにかする

--- src/components/Web/GetTags/GetTags.tsx
+++ src/components/Web/GetTags/GetTags.tsx
@@ -10,7 +10,7 @@ interface TagProps {
     logoname: string;
 }
 
-const Tags = [
+const Tags: TagProps[] = [
     {
         logourl: "./ts-logo-128.png",
         logoname: "typescript",
@@ -20,67 +20,17 @@ const Tags = [
         logoname: "blender",
     },
 ];
-    // <Grid container md="auto" >
-    //     <Image alt="image" src={logourl} key={index}/>
-    //     <Typography textAlign={"center"} ml={2} marginTop="8px">
-    //         {logoname}
-    //     </Typography>
-    // </Grid>
+export const GetTags = (Tags:TagProps) => {
     return (
-        {Tags.map((Tags:TagProps, index:number) => (
-                <Grid container md="auto">
-                    <Image alt="image" src={Tags.logourl} key={index} />
-                    <Typography ml={2} marginTop="8px">
-                        {Tags.logoname}
-                    </Typography>
-                </Grid>
+        {
+            Tags.map((Tags:TagProps, index: number) => (
+                    <Grid container md="auto" key={index}>
+                        <Image alt="image" src={Tags.logourl} />
+                        <Typography textAlign={"center"} ml={2} marginTop="8px">
+                            {Tags.logoname}
+                        </Typography>
+                    </Grid>
             ))
         }
-    );
-
-
-/* import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-
-function srcset(image: string, size: number, rows = 1, cols = 1) {
-    return {
-        src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-        srcSet: `${image}?w=${size * cols}&h=${
-            size * rows
-        }&fit=crop&auto=format&dpr=2 2x`,
-    };
+    )
 }
-
-export default function QuiltedImageList() {
-    return (
-        <ImageList
-            sx={{ width: 500, height: 450 }}
-            variant="quilted"
-            cols={4}
-            rowHeight={121}
-        >
-            {TagData.map((item) => (
-                <ImageListItem
-                    key={item.img}
-                >
-                    <img
-                        {...srcset(item.img, 121, item.rows, item.cols)}
-                        alt={item.title}
-                        loading="lazy"
-                    />
-                </ImageListItem>
-            ))}
-        </ImageList>
-    );
-}
-
-const TagData = [
-    {
-        img: "./ts-logo-128.png",
-        title: "typescript",
-    },
-    {
-        img: "./blender.png",
-        title: "blender",
-    },
-]; */
