36a7d6030915ec93d69f40e47532a66646ab4a0a
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-04-25 19:29:34 +0900

    とりあえずmapどうにかする

---
+++ src/components/Web/GetTags/Tags.tsx
@@ -0,0 +1,15 @@
+interface TagProps {
+    logourl: string;
+    logoname: string;
+}
+
+export const Tags: TagProps[] = [
+    {
+        logourl: "./ts-logo-128.png",
+        logoname: "typescript",
+    },
+    {
+        logourl: "./blender.png",
+        logoname: "blender",
+    },
+];
