f436bfdd1f953eaee58cf7b7ee9cb174d2ef3b99
tosaken <v-ntosa@alterbooth.net>
2023-04-27 19:02:16 +0900

    add:ロード画像の表示 #8

---
+++ src/components/Common/othres/Layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TopLoading } from "../TopLoading/TopLoading";
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <>{children}</>
+            <TopLoading />
+        </>
+    );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
+import Layout from "@/components/Common/othres/Layout";
 import { createTheme } from "@/libs/theme";
 import { ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
     const theme = createTheme();
     return (
-        <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <RecoilRoot>
+            <ThemeProvider theme={theme}>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ThemeProvider>
+        </RecoilRoot>
     );
 }
 
