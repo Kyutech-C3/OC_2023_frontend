ab3c83d6bf1ff657826eb165bcdb14d061092ded
tosaken <v-ntosa@alterbooth.net>
2023-05-06 02:38:25 +0900

    add:ユーザーカードの作成 #50

---
+++ src/components/Web/User/UserCard.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { UserCard } from "./UserCard";
+
+export default {
+    title: "Web/User",
+    component: UserCard,
+    tags: ["autodocs"],
+    parameters: {
+        layout: "fullscreen",
+    },
+    argTypes: {},
+} as ComponentMeta<typeof UserCard>;
+
+const Template: ComponentStory<typeof UserCard> = (args) => (
+    <UserCard {...args} />
+);
+export const Default = Template.bind({});
+Default.args = {
+    display_name: "土佐犬",
+    avatar_url:
+        "https://cdn.discordapp.com/avatars/506014633665101825/45768a9214cc66dc474eb884cf31be68.png",
+};
---
+++ src/components/Web/User/UserCard.tsx
@@ -0,0 +1,17 @@
+import { User as UserProps } from "@/types/common";
+import { Avatar, Stack, Typography } from "@mui/material";
+
+export const UserCard = ({
+    display_name: disPlayName,
+    avatar_url: avatarUrl,
+}: Pick<UserProps, "display_name" | "avatar_url">) => {
+    console.log(disPlayName);
+    return (
+        <Stack direction="row" spacing={2}>
+            <Avatar src={avatarUrl} sx={{ border: "1px solid #aaaaaadd" }} />
+            <Typography variant="h6" sx={{ alignSelf: "center" }}>
+                {disPlayName}
+            </Typography>
+        </Stack>
+    );
+};
