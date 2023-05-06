e9c06db1bcdbdc570022c54e6745bae251008b94
Sea10wood <miyusea5@gmail.com>
2023-05-07 03:03:34 +0900

    add:データ取得の実装 #63

--- src/pages/web/artifact/index.tsx
+++ src/pages/web/artifact/index.tsx
@@ -1,4 +1,25 @@
-const Artifact = () => {
-    return <>this is artifact page</>;
+import { Artifact } from "@/components/Web/Artifact/Artifact";
+import { useTopLoading } from "@/hooks/common";
+import { Work } from "@/types/common";
+import { Grid } from "@mui/material";
+import axios from "axios";
+import useSWR from "swr";
+
+const Artifacts = () => {
+    const fetcher = (url: string) => axios(url).then((res) => res.data.works);
+    const { data, isLoading } = useSWR(
+        `${process.env.NEXT_PUBLIC_BACKEND_API}/api/v1/works`,
+        fetcher
+    );
+    useTopLoading({ isLoading, message: "loading" });
+    return (
+        <Grid container>
+            {data?.map((artifact: Work, index: number) => (
+                <Grid item key={index}>
+                    <Artifact {...artifact} />
+                </Grid>
+            ))}
+        </Grid>
+    );
 };
-export default Artifact;
+export default Artifacts;
