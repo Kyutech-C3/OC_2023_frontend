e0f5157de587b4eeaa4a1e06d50493da525dabaf
tosaken <v-ntosa@alterbooth.net>
2023-05-06 18:57:26 +0900

    fix:storiesの修正 #23

--- src/components/Web/Artifact/Artifact.stories.tsx
+++ src/components/Web/Artifact/Artifact.stories.tsx
@@ -45,6 +45,15 @@ Default.args = {
         extension: "png",
         created_at: "2023-05-03T17:47:56.186760",
         asset_type: "image",
+        user: {
+            id: "e41601a1-9efe-4b7e-a946-55f7a1df6ef7",
+            name: "土佐犬",
+            display_name: "土佐犬",
+            avatar_url:
+                "https://cdn.discordapp.com/avatars/506014633665101825/45768a9214cc66dc474eb884cf31be68.png",
+            created_at: "2023-05-03T17:44:49.962784",
+            updated_at: "2023-05-03T17:44:49.962784",
+        },
         url: "https://s3.ap-northeast-2.wasabisys.com/toybox/assets/image/64bdb5d5-8067-471e-916c-e8ab58394ba2/origin.png",
         updated_at: "2023-05-03T17:47:56.195082",
     },
