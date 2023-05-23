7baa63e1fcf16297679fefeecbd09062d9a0e40e
Aopaka1231 <hiroyuki_kuroiwa@icloud.com>
2023-05-24 00:50:16 +0900

    add:デフォルトアイコンの追加

--- src/components/Web/GetTags/WorkTag.tsx
+++ src/components/Web/GetTags/WorkTag.tsx
@@ -1,18 +1,23 @@
 import { GetTag } from "@/types/common";
 import { Games } from "@mui/icons-material";
+import BrowserNotSupportedIcon from "@mui/icons-material/BrowserNotSupported";
 import ColorLensIcon from "@mui/icons-material/ColorLens";
 import FunctionsIcon from "@mui/icons-material/Functions";
 import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
 import ShapeLineIcon from "@mui/icons-material/ShapeLine";
 import TerminalIcon from "@mui/icons-material/Terminal";
 import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
 
+import LocalOfferOutlinedIcon from "@mui/icons-material/LocalOfferOutlined";
 import ImageIcon from "./ImageIcon";
 
-const tag2icon = {
+const defaultIcon: React.ReactNode = <BrowserNotSupportedIcon />;
+
+const tag2icon: Record<string, React.ReactNode | undefined> = {
     イラスト: <ColorLensIcon />,
-    C3公式グッズデザイン案: <ImageIcon src="/images/icons/C3logo.png" />,
-    GWクリエイタソン: "",
+    C3公式グッズデザイン案: <ImageIcon src="/public/image/icon/C3logo.jpg" />,
+    GWクリエイタソン: <LocalOfferOutlinedIcon />,
     cg: <SettingsSuggestIcon />,
     Unity: <ImageIcon src="/image/icon/unity.png" />,
     モデル_3D: <ImageIcon src="/image/icon/3d_model.png" />,
@@ -25,6 +30,7 @@ const tag2icon = {
 };
 
 export function WorkTag({ name }: GetTag) {
+    const iconComponent = tag2icon[name] || defaultIcon;
     return (
         <>
             <Box sx={{ width: "fit-content" }}>
@@ -38,7 +44,7 @@ export function WorkTag({ name }: GetTag) {
                     }}
                     spacing={1}
                 >
-                    {tag2icon[name]}
+                    {iconComponent}
                     <Typography>{name}</Typography>
                 </Stack>
             </Box>
