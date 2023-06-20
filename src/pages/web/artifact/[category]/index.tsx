8e0e0767d033b768dde8bf714171ef1a8376508d
tosaken1116 <nagato20021122@gmail.com>
2023-06-20 13:34:51 +0900

    add:各カテゴリのページの作成 #71

--- src/pages/web/artifact/[category]/index.tsx
+++ src/pages/web/artifact/[category]/index.tsx
@@ -1,8 +1,54 @@
+import { Artifact } from "@/components/Web/Artifact/Artifact";
+import { useTopLoading } from "@/hooks/common";
+import { useGetWorks } from "@/hooks/web";
+import { Work } from "@/types/common";
+import { Box, Grid } from "@mui/material";
 import { useRouter } from "next/router";
+import InfiniteScroll from "react-infinite-scroller";
 
 const CategoryTop = () => {
     const router = useRouter();
-    return <>this is {router.query.category} top</>;
+    const { category } = router.query;
+    if (
+        !["game", "hack", "music", "cg2d", "cg3d"].includes(
+            typeof category == "undefined"
+                ? ""
+                : typeof category == "string"
+                ? category
+                : category[0]
+        )
+    ) {
+        if (router.isReady) {
+            router.push("/web/artifact");
+        }
+    }
+    const { works, refetch, isLoading, isContinue } = useGetWorks(
+        `${process.env.NEXT_PUBLIC_BACKEND_API}/api/v1/works?tag_names=${category}`
+    );
+    useTopLoading({ isLoading, message: "getting" });
+    return (
+        <Box component="div" sx={{ mt: 5 }}>
+            <InfiniteScroll
+                loadMore={() => {
+                    if (works.length != 0) {
+                        refetch(
+                            `newest_work_id=${works[works.length - 1].id}`,
+                            false
+                        );
+                    }
+                }}
+                hasMore={isContinue}
+            >
+                <Grid container justifyContent={"center"} spacing={3}>
+                    {works?.map((artifact: Work, index: number) => (
+                        <Grid item key={index}>
+                            <Artifact {...artifact} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </InfiniteScroll>
+        </Box>
+    );
 };
 
 export default CategoryTop;
