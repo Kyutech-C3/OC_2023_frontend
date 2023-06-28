5fc11e266a119cc05c231468901da4bb91e419a7
tosaken1116 <nagato20021122@gmail.com>
2023-06-29 01:27:29 +0900

    add:ボタンを押した時のスクロール機能の追加 #105

--- src/components/Web/ScrollTo/ScrollTo.tsx
+++ src/components/Web/ScrollTo/ScrollTo.tsx
@@ -1,4 +1,5 @@
 import { Button, useMediaQuery } from "@mui/material";
+import { Link as Scroll } from "react-scroll";
 
 export type ScrollToProps = {
     to: string;
@@ -7,39 +8,43 @@ export type ScrollToProps = {
 export const ScrollTo = ({ to }: ScrollToProps) => {
     const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
     return (
-        <Button
-            sx={{
-                mixBlendMode: "multiply",
-                color: isDarkMode ? "white" : "black",
-                ":after": {
-                    content: '""',
-                    background: `linear-gradient(to top,${
-                        isDarkMode ? "white" : "black"
-                    } 25%,transparent 25%,transparent 75%,${
-                        isDarkMode ? "white" : "black"
-                    } 75%)`,
+        <Scroll to={to} smooth={true} duration={800}>
+            <Button
+                sx={{
                     position: "absolute",
-                    backgroundPosition: "center bottom",
-                    width: "2%",
-                    height: "10vh",
-                    left: "48%",
-                    top: "100%",
-                    backgroundSize: "auto 400%",
-                    transition: "0.5s",
-                },
-                ":hover:after": {
-                    backgroundPosition: "center top",
-                },
-                ":before": {
-                    content: '""',
-                    position: "absolute",
-                    width: "60%",
-                    height: "100vh",
-                    background: "transparent",
-                },
-            }}
-        >
-            scroll
-        </Button>
+                    left: "50%",
+                    bottom: "10vh",
+                    color: isDarkMode ? "white" : "black",
+                    ":after": {
+                        content: '""',
+                        background: `linear-gradient(to top,${
+                            isDarkMode ? "white" : "black"
+                        } 25%,transparent 25%,transparent 75%,${
+                            isDarkMode ? "white" : "black"
+                        } 75%)`,
+                        position: "absolute",
+                        backgroundPosition: "center bottom",
+                        width: "2%",
+                        height: "10vh",
+                        left: "48%",
+                        top: "100%",
+                        backgroundSize: "auto 400%",
+                        transition: "0.5s",
+                    },
+                    ":hover:after": {
+                        backgroundPosition: "center top",
+                    },
+                    ":before": {
+                        content: '""',
+                        position: "absolute",
+                        width: "60%",
+                        height: "100vh",
+                        background: "transparent",
+                    },
+                }}
+            >
+                scroll
+            </Button>
+        </Scroll>
     );
 };
