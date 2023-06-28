e2fd11360b2906cd4a8179333645de5c523a135f
tosaken1116 <nagato20021122@gmail.com>
2023-06-29 02:47:39 +0900

    add:トップページのタイトル背景画像の作成 #108

---
+++ src/constants/topBackground.ts
@@ -0,0 +1,27 @@
+export const imagePaths = [
+    {
+        department: "hack",
+        imagePath:
+            "https://img.freepik.com/free-photo/cute-kitten-staring-out-the-window-playful-curiosity-generative-ai_188544-12520.jpg",
+    },
+    {
+        department: "game",
+        imagePath:
+            "https://img.freepik.com/free-photo/cute-kitten-staring-out-the-window-playful-curiosity-generative-ai_188544-12520.jpg",
+    },
+    {
+        department: "2dcg",
+        imagePath:
+            "https://img.freepik.com/free-photo/cute-kitten-staring-out-the-window-playful-curiosity-generative-ai_188544-12520.jpg",
+    },
+    {
+        department: "3dcg",
+        imagePath:
+            "https://img.freepik.com/free-photo/cute-kitten-staring-out-the-window-playful-curiosity-generative-ai_188544-12520.jpg",
+    },
+    {
+        department: "music",
+        imagePath:
+            "https://img.freepik.com/free-photo/cute-kitten-staring-out-the-window-playful-curiosity-generative-ai_188544-12520.jpg",
+    },
+];
