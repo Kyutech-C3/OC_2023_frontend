7fcf1ea837a7646ba3d8c8215ec6bfd351d4b993
okaka8080 <lg.star1208@gmail.com>
2023-06-28 11:52:00 +0900

    add:会場モデルの表示 #12

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,4 +1,42 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Site } from "../../components/Map/Site/Site";
+import { Plane } from "../../components/Map/Plane"
+import styles from "../../styles/map.module.css";
+import { Physics, useBox } from "@react-three/cannon";
+
+function Box(props: any) {
+    const [ref, api] = useBox(() => ({
+        mass: 1,
+        position: [30, 10, 0.8],
+        args: [1, 1, 1],
+    }));
+
+    return (
+        <mesh ref={ref} onClick={() => api.velocity.set(0, 5, 0)} {...props}>
+            <boxBufferGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+    );
+}
+
 const Map = () => {
-    return <>this is map</>;
+    return (
+        <div className={styles.map}>
+            <Canvas camera={{ position: [0, 2, -1] }} shadows style={{ height: "100%" }} className={styles.canvas}>
+                <Physics
+                    broadphase="SAP"
+                    gravity={[0, -2.6, 0]}
+                >
+                    <directionalLight position={[0, 10, 0]} castShadow />
+                    <Plane />
+                    <Box />
+                    <Site />
+                    <OrbitControls target={[0, 0, 0]} />
+                </Physics>
+            </Canvas>
+        </div>
+    );
 };
+
 export default Map;
