b23dbbc201319f0ec5343f0793b487e00b0df865
FUKUDA Miyu <126382452+Sea10wood@users.noreply.github.com>
2023-07-08 10:26:44 +0900

    fix: MessageTitle-Fixed#32

--- src/components/Web/MessageTitle/MessageTitle.tsx
+++ src/components/Web/MessageTitle/MessageTitle.tsx
@@ -1,61 +1,60 @@
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 
 const MessageTitle = () => {
-    const characters = [
-        {
-            char: "M",
-            color: "#64F161",
-            fontSize: "13vw",
-        },
-        {
-            char: "e",
-            color: "#FFE600",
-            fontSize: "11vw",
-        },
-        {
-            char: "s",
-            color: "#7AFFA7",
-            fontSize: "9vw",
-        },
-        {
-            char: "s",
-            color: "#FFB800",
-            fontSize: "8vw",
-        },
-        {
-            char: "a",
-            color: "#BCFFD3",
-            fontSize: "6vw",
-        },
-        {
-            char: "g",
-            color: "#FFE587",
-            fontSize: "5vw",
-        },
-        {
-            char: "e",
-            color: "#BCFFD3",
-            fontSize: "4vw",
-        },
-    ];
+  const characters = [
+    {
+      char: "M",
+      color: "#64F161",
+      fontSize: "13vw",
+    },
+    {
+      char: "e",
+      color: "#FFE600",
+      fontSize: "11vw",
+    },
+    {
+      char: "s",
+      color: "#7AFFA7",
+      fontSize: "9vw",
+    },
+    {
+      char: "s",
+      color: "#FFB800",
+      fontSize: "8vw",
+    },
+    {
+      char: "a",
+      color: "#BCFFD3",
+      fontSize: "6vw",
+    },
+    {
+      char: "g",
+      color: "#FFE587",
+      fontSize: "5vw",
+    },
+    {
+      char: "e",
+      color: "#BCFFD3",
+      fontSize: "4vw",
+    },
+  ];
 
-    return (
-        <>
-            {characters.map(({ char, color, fontSize,fontWeight }, index) => (
-                <Typography
-                    key={index}
-                    style={{
-                        color: color,
-                        fontSize: fontSize,
-                        direction="row",
-                        fontWeight: "bold",
-                    }}
-                >
-                    {char}
-                </Typography>
-            ))}
-        </>
-    );
+  return (
+    <Box display="flex">
+      {characters.map(({ char, color, fontSize, fontWeight }, index) => (
+        <Typography
+          key={index}
+          style={{
+            color: color,
+            fontSize: fontSize,
+            fontWeight: "bold",
+          }}
+        >
+          {char}
+        </Typography>
+      ))}
+    </Box>
+  );
 };
 
 export default MessageTitle;
