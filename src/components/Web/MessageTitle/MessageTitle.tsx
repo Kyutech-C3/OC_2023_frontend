b1a7b2ab3af02f66b9aeac007bcf7b20d812a50d
Sea10wood <seamiyu5@gmail.com>
2023-07-08 14:21:01 +0900

    fix:MessageTitle-#32

--- src/components/Web/MessageTitle/MessageTitle.tsx
+++ src/components/Web/MessageTitle/MessageTitle.tsx
@@ -1,60 +1,62 @@
-import { Typography, Box } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 const MessageTitle = () => {
-  const characters = [
-    {
-      char: "M",
-      color: "#64F161",
-      fontSize: "13vw",
-    },
-    {
-      char: "e",
-      color: "#FFE600",
-      fontSize: "11vw",
-    },
-    {
-      char: "s",
-      color: "#7AFFA7",
-      fontSize: "9vw",
-    },
-    {
-      char: "s",
-      color: "#FFB800",
-      fontSize: "8vw",
-    },
-    {
-      char: "a",
-      color: "#BCFFD3",
-      fontSize: "6vw",
-    },
-    {
-      char: "g",
-      color: "#FFE587",
-      fontSize: "5vw",
-    },
-    {
-      char: "e",
-      color: "#BCFFD3",
-      fontSize: "4vw",
-    },
-  ];
+    const characters = [
+        {
+            char: "M",
+            color: "#64F161",
+            fontSize: "11vw",
+        },
+        {
+            char: "E",
+            color: "#FFE600",
+            fontSize: "10vw",
+        },
+        {
+            char: "S",
+            color: "#7AFFA7",
+            fontSize: "9vw",
+        },
+        {
+            char: "S",
+            color: "#FFB800",
+            fontSize: "8vw",
+        },
+        {
+            char: "A",
+            color: "#BCFFD3",
+            fontSize: "7vw",
+        },
+        {
+            char: "G",
+            color: "#FFE587",
+            fontSize: "6vw",
+        },
+        {
+            char: "E",
+            color: "#BCFFD3",
+            fontSize: "5vw",
+        },
+    ];
 
-  return (
-    <Box display="flex">
-      {characters.map(({ char, color, fontSize, fontWeight }, index) => (
-        <Typography
-          key={index}
-          style={{
-            color: color,
-            fontSize: fontSize,
-            fontWeight: "bold",
-          }}
-        >
-          {char}
-        </Typography>
-      ))}
-    </Box>
-  );
+    return (
+        <Stack direction="row" sx={{ alignItems: "baseline" }}>
+            {characters.map(({ char, color, fontSize }, index) => (
+                <Typography
+                    key={index}
+                    style={{
+                        color: color,
+                        fontSize: fontSize,
+                        height: fontSize,
+                        fontWeight: "bold",
+                        display: "flex",
+                    }}
+                >
+                    {char}
+                </Typography>
+            ))}
+        </Stack>
+    );
 };
 
 export default MessageTitle;
