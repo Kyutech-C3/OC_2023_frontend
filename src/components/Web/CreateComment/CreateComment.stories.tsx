53d9d540183be31fbc2f4178d63f43d94fabd82c
tosaken1116 <nagato20021122@gmail.com>
2023-07-11 17:00:25 +0900

    add:コメントを投稿できるように実装 #122

--- src/components/Web/CreateComment/CreateComment.stories.tsx
+++ src/components/Web/CreateComment/CreateComment.stories.tsx
@@ -3,14 +3,18 @@ import { Meta, StoryObj } from "@storybook/react";
 import { CreateComment } from "./CreateComment";
 
 export default {
-  title: "Web/CreateComment",
-  component: CreateComment,
-  tags: ['autodocs'],
-  parameters: {
-    layout: 'fullscreen',
-  },
-  argTypes: {
-  },
+    title: "Web/CreateComment",
+    component: CreateComment,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+    argTypes: {
+    },
 } as Meta<typeof CreateComment>;
 
-export const Default: StoryObj<typeof CreateComment> = {};
+export const Default: StoryObj<typeof CreateComment> = {
+    args: {
+        workId: "1a75b460-2966-4650-9a5c-55554277016f"
+    }
+};
--- src/components/Web/CreateComment/CreateComment.tsx
+++ src/components/Web/CreateComment/CreateComment.tsx
@@ -1,12 +1,34 @@
+import { getUserId } from '@/libs/getUserId';
 import SendIcon from '@mui/icons-material/Send';
 import { Box, Button, Stack, TextareaAutosize, TextField } from "@mui/material";
-export const CreateComment = () => {
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+export const CreateComment = ({ workId }: { workId: string }) => {
+    const [userId, setUserId] = useState("")
+    const [formValue, setFormValue] = useState({ userName: "名無し", comment: "" })
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setFormValue({ ...formValue, [event.target.name]: event.target.value })
+    }
+    useEffect(() => {
+        setUserId(getUserId())
+    }, [])
+    const handleComment = async () => {
+        if (formValue.comment === "") {
+            return
+        }
+        try {
+            await axios.post(process.env.NEXT_PUBLIC_BACKEND_API! + "/api/v1/comments", { "user_id": userId, "work_id": workId, "user_name": formValue.userName, "comment": formValue.comment });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
     return (
         <Stack spacing={2} sx={{ p: 2, borderRadius: "10px", backgroundColor: "primary.light" }}>
-            <TextField label="ユーザー名" defaultValue={"名無し"} sx={{ backgroundColor: "#ffffff88", borderRadius: "5px", }} color="secondary" />
-            <TextareaAutosize style={{ height: "200px", borderRadius: "10px", backgroundColor: "#ffffff88" }} />
+            <TextField name="userName" onChange={handleChange} label="ユーザー名" defaultValue={formValue.userName} sx={{ backgroundColor: "#ffffff88", borderRadius: "5px", }} color="secondary" />
+            <TextareaAutosize onChange={handleChange} name="comment" style={{ height: "200px", borderRadius: "10px", backgroundColor: "#ffffff88" }} />
             <Box component="div" alignSelf="end">
-                <Button variant="contained" color="secondary" startIcon={<SendIcon />} > 送信</Button>
+                <Button variant="contained" color="secondary" startIcon={<SendIcon />} onClick={handleComment} > 送信</Button>
             </Box>
         </Stack >
     );
